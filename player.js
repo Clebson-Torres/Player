d3209ae0d98454e08922653883d9eb95ae78aee0
Clebson Torres <clebson.torres@gmail.com>
2022-09-03 14:23:59 -0300

    player stable commit

--- player.js
+++ player.js
@@ -1,8 +1,20 @@
+let musicas = [
+    { titulo: 'reggae solo', artista: 'zeka', src: 'musicas/Two Sides - SefChol.mp3', img: 'imagens/1.jpg' },
+    { titulo: 'reggae music', artista: 'zika', src: 'musicas/We Ride! - Reed Mathis.mp3', img: 'imagens/2.jpg' },
+    { titulo: 'leve com vc', artista: 'Natiruts', src: 'musicas/Natiruts – Leve Com Você (Ao Vivo).mp3', img: 'imagens/3.jpg' }
+]
+
 let musica = document.querySelector('audio');
+
+let indexMusica = 0;
+
 let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao p');
 
 
-
+renderizarMusica(indexMusica);
 
 
 
@@ -11,39 +23,64 @@ document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
-musica.addEventListener('timeupdate',atualiarBarra);
+musica.addEventListener('timeupdate', atualizarBarra);
 
-window.onload = duration;
 
+
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
 
 // funcoes
-function tocarMusica(){
+function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src);
+    pausarMusica();
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+
+function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
     document.querySelector('.botao-play').style.display = 'none';
 }
 
-function pausarMusica(){
+function pausarMusica() {
     musica.pause();
     document.querySelector('.botao-pause').style.display = 'none';
     document.querySelector('.botao-play').style.display = 'block';
 }
 
-function atualiarBarra(){
+function atualizarBarra() {
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
-function segundosParaMinutos(segundos){
+function segundosParaMinutos(segundos) {
     let campoMInutos = Math.floor(segundos / 60);
     let campoSegundos = segundos % 60;
-    if (campoSegundos< 10){
+    if (campoSegundos < 10) {
         campoSegundos = '0' + campoSegundos;
     }
-    return campoMInutos+':'+campoSegundos;
-}
-function duration(){
-    durationMusic.textContent = secondsForMinutes(Math.floor(currentSong.duration))
+    return campoMInutos + ':' + campoSegundos;
 }
